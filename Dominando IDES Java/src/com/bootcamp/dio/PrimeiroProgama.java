d413522b128b097cbbc0d41df8250d8ee8051fe4
girlenolima <girlenosilveira@gmail.com>
2021-12-14 22:31:19 -0300

    Add dir ides java e variaveis

---
+++ Dominando IDES Java/src/com/bootcamp/dio/PrimeiroProgama.java
@@ -0,0 +1,17 @@
+package com.bootcamp.dio;
+
+import com.bootcamp.dio.model.gato.Gato;
+
+public class PrimeiroProgama {
+
+	public static void main(String[] args) {
+
+		System.out.println("Ola Mundo");
+
+		Gato gato = new Gato("Pancadao", "Amarelo", "15");
+
+		System.out.println(gato);
+
+	}
+
+}
